bf1f34c8f1d8ba65ab71d2efef555cdcf596a2cc
junyoung <no2jfamily@tukorea.ac.kr>
2023-05-03 23:47:10 +0900

    feat: main화면 css 적용, GlobalStyle로 css 전역적으로 적용

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,22 @@
+import styled from 'styled-components';
+
+const Wrapper = styled.div`
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    background-color: rgb(240, 235, 248);
+`;
+
+
+
+
 function App() {
-    return <div/>
+    return (
+        <Wrapper>
+            hi
+        </Wrapper>
+    )
 }
 
 export default App
