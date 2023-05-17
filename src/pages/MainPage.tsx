c73ded4262d3916452b58df6eee832f705b6e9bd
junyoung <no2jfamily@tukorea.ac.kr>
2023-05-17 14:46:09 +0900

    feat: MainPage 구현중

---
+++ src/pages/MainPage.tsx
@@ -0,0 +1,22 @@
+import styled from 'styled-components';
+
+const Wrapper = styled.div`
+    margin-left: auto;
+    margin-right: auto;
+    min-height: 100vh;
+    width: 100%;
+    max-width: 820px;
+    border-width: 8px;
+    border-color: rgb(209 213 219);
+    padding: 2.5rem;
+`;
+
+const MainPage = () => {
+    return (
+        <>
+            <Wrapper>hi</Wrapper>
+        </>
+    );
+};
+
+export default MainPage;
